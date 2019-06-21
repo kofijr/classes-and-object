016555c07d5b7dc366f28f357d14e03f2b33a2c8
kofijr <calebasanteawuah@gmail.com>
2019-06-21 16:28:08 +0000

    pratice test 2

---
+++ Holiday.java
@@ -0,0 +1,24 @@
+public class Holiday {
+	  private String name;
+  	private int day;
+		private String month;
+	}
+	public Holiday(String NAME, int DAY, String MONTH) {
+	   name = NAME;
+		 day = DAY;
+		 month = MONTH;
+	 }
+	 public boolean inSameMonth(Holiday holiday)
+	 {
+		  return this.month.equals(holiday.month);
+	 }
+	 public static double avgDate(Holiday[] holiday) 
+	 { int sum = 0;
+		 for(int i = 0; i < holiday.length; i++)
+		 {  sum = sum + holiday[i].day;
+		 }
+		 return ((double) sum)/holiday.leng
+	 }
+
+
+	 Holiday hol = new Holiday(“Independence Day”, 4, “July”);
